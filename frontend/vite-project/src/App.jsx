707efdc8187e8de58d7e9603a46c6394e4ea7a5b
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-08-11 20:20:31 +0530

    update

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+
+function App() {
+ 
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline"> Hello World </h1>
+    </>
+  )
+}
+
+export default App

cffd3f2c5de5602a501cb8e023693dabacc67ef1
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-08-12 20:28:42 +0530

    update

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,12 +1,11 @@
 // import { useState } from 'react'
-
+import './App.css';
+import Layout from './layout/Layout';
 
 function App() {
  
   return (
-    <>
-       <h1 className="text-3xl font-bold underline"> Hello World </h1>
-    </>
+    <Layout/>
   )
 }
 
---
+++ frontend/vite-project/src/components/Doctors/DoctorCard.jsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+// // import { doctors } from '../../assets/data/docters'
+
+// const DoctorCard = ({doctor}) => {
+
+//   const {
+//     name, 
+//     avgRating, 
+//     totalRating, 
+//     photo, 
+//     specialization, 
+//     totalPatients, 
+//     hospital
+//   } = doctor;
+
+//   return (
+//     <div className='p-3 lg:p-5'>
+//       <div>
+//         <img src={photo} alt='' className='w-full'/>
+//       </div>
+//       <h2 className='text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-headingClor font-[700] mt-3 lg:mt-5'></h2>
+//     </div>
+//   )
+// }
+
+// export default DoctorCard
+
+
+import React from 'react'
+
+const DoctorCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorCard

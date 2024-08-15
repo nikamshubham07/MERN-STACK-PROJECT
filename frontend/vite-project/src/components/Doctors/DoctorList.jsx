5c37e74a52bedac3699eb4d024fc07a3d2a98705
Shubham Nikam <132605738+nikamshubham07@users.noreply.github.com>
2024-08-15 22:34:21 +0530

    Update DoctorList.jsx

--- frontend/vite-project/src/components/Doctors/DoctorList.jsx
+++ frontend/vite-project/src/components/Doctors/DoctorList.jsx
@@ -4,7 +4,6 @@ import {doctors} from './../../assets/data/doctors';
 import DoctorCard from './DoctorCard';
 
 const DoctorList = () => {
-  // console.log(doctors);
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] '>
       {doctors.map(doctor => (

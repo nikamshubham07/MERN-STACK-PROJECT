e4ce0c80455254eb2fd83a7334f7191df598c6f3
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-08-16 20:05:50 +0530

    update

---
+++ frontend/vite-project/src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { formateDate } from '../../utils/formateDate'
+
+const DoctorAbout = () => {
+  return (
+    <div>
+        <div className="">
+            <h3 className="text-[20px] leading-[30px] text-headingClor font-semibold flex items-center gap-2">About of
+                <span className='text-irisBlueColor font-bold text-[24px] leading-9'>Muhibur Rahman</span>
+            </h3>
+            <p className="text__para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt tempore minima illo repudiandae velit facilis facere vel quas rerum eligendi repellat sunt ipsa quos delectus, placeat magnam odio, rem nihil illo repudiandae velit facilis facere vel quas rerum eligendi repellat sunt ipsa quos delectus, placeat magnam odio, rem nihil !
+            </p>
+        </div>
+        <div className="mt-12">
+            <h3 className="text-[20px] leading-[30px] text-headingClor font-semibold">Education</h3>
+
+            <ul className='pt-4 md:p-5'>
+               <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                    <div className="">
+                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                            {formateDate("10-06-2012")} -{formateDate("10-06-2014")}
+                        </span>
+                        <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeno</p>
+                    </div>
+                    <p className='text-[14px] leading-5 font-medium text-textColor'>New Apollo Hospital, New York.</p>
+               </li>
+
+               <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                    <div className="">
+                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                            {formateDate('12-04-2010')} - {formateDate("8-05-2012")}
+                        </span>
+                        <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeno</p>
+                    </div>
+                    <p className='text-[14px] leading-5 font-medium text-textColor'>New Apollo Hospital, New York.</p>
+               </li>
+
+            </ul>
+        </div>
+
+        <div className='mt-12'>
+            <h3 className="text-[20px] leading-[30px] text-headingClor font-semibold">Experience</h3>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default DoctorAbout
---
+++ frontend/vite-project/src/pages/Doctors/FeedBack.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const FeedBack = () => {
+  return (
+    <div>
+      FeedBack
+    </div>
+  )
+}
+
+export default FeedBack

487287b79e042a58522d59f84e00f20ae3df10d3
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-08-22 12:47:42 +0530

    update

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -33,7 +33,7 @@ export const getSingleDoctor = async(req, res)=>{
     const id = req.params.id
 
     try{
-        const doctor = await Doctor.findById(id).select("-password");
+        const doctor = await Doctor.findById(id).populate('reviews').select("-password");
 
         res.status(200).json({sucess:true, message:"User found", data:doctor})
     }

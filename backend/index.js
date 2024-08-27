8fe6c24b4ca84afb61ea653fb301ecde742bb53e
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-08-27 17:34:10 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const connectDB = async()=>{
     try{
          await mongoose.connect(process.env.MONGO_URL, {
             // useNewUrlParser: true,
-            // useUnifiedTopology: true,
+            // useUnifiedTopology: true, 
         })
 
         console.log('mongoDB database is connected')

efb9d2d1c43091e3cc19ec5e01741f3c31edacb8
Shubham Nikam <shubhamarunnikam2003@gmail.com>
2024-09-22 23:30:38 +0530

    build code

--- frontend/vite-project/src/config.js
+++ frontend/vite-project/src/config.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL = 'http://localhost:5001/api/v1'
+export const BASE_URL = 'https://mern-stack-project-1-i9g4.onrender.com/api/v1'
 export const token =localStorage.getItem("token");
